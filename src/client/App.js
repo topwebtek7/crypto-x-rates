3ae99451042fcec25d524b2ac068b63c446ee766
Brian <brianwebtek@gmail.com>
2018-05-20 23:03:09 +0800

    Add xrates class, Fix UI/UX

--- src/client/App.js
+++ src/client/App.js
@@ -4,20 +4,46 @@ import './app.css';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: null };
+    this.state = { data: null };
+    this.tick = this.tick.bind(this)
   }
 
-  componentDidMount() {
-    fetch('/api/getUsername')
+  tick() {
+    fetch('/api/cryptoData')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(data => this.setState({ data: data }))
+  }
+
+  componentDidMount(){
+    this.tick()
+    this.interval = setInterval(this.tick, 60000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
+    const tableHeaders = (<thead>
+      <tr>
+          <th>Cryptocurrency</th>
+          <th>Symbol</th>
+          <th>Price(USD)</th>
+          <th>Updated</th>
+      </tr>
+    </thead>);
+
     return (
-      <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
+      <div className='crypto-table-container'>
+        {this.state.data ? (
+        <table className='crypto-table' width='100%'>
+          {tableHeaders}
+          <tbody>
+            {this.state.data.map(e => (
+              <tr><td>{e['name']}</td><td>{e['symbol']}</td><td>{e['usd']['price']}</td><td>{e['last_updated']}</td></tr>
+            ))}
+          </tbody>
+        </table>
         ) : (
           <h1>Loading.. please wait!</h1>
         )}
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,18 @@
+"use strict";
+
 const express = require('express');
 const os = require('os');
+const CryptoXRates = require('./cryptoXRatesClass')
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+const newCryptoXRates = new CryptoXRates();
+
+app.get('/api/cryptoData', (req, res) => {
+    newCryptoXRates.rateList()
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(400).send(JSON.parse(err)))
+});
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
